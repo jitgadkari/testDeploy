996e48f34b8cb2440503d363ba003b18fddc3a03
Ajit Shatvapa Gadkari <ajit@Ajits-MacBook-Pro.local>
2023-12-06 17:22:44 +0530

    change

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const cors=require("cors");
 const app=express();
 app.use(cors());
 const PORT=9000;
-app.use(("/",(req,resp)=>{
+app.use("/",(req,resp)=>{
     resp.send("server running");
-}))
+})
 
 app.listen(PORT,()=>{
     console.log("listening on port"+PORT);
